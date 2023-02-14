5e2c395cebf7f670b87ce75e8ac8992a097f5f78
DmitrievaKseniya <dkn1933@mail.ru>
2023-02-14 20:06:01 +0400

    Решение задания 12.1.5

--- Task12_1_5/Program.cs
+++ Task12_1_5/Program.cs
@@ -4,7 +4,60 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Заполним список из этих пользователей
+            var listUser = GetListUser();
+
+            foreach (User user in listUser)
+            {
+                Console.WriteLine($"Привет, {user.Name}\n");
+                if (!user.IsPremium)
+                {
+                    Console.WriteLine("***РЕКЛАМНАЯ ПАУЗА***");
+                    ShowAds();
+                    Console.WriteLine("***РЕКЛАМА ЗАКОНЧИЛАСЬ***\n");
+                }
+            }
+        }
+        static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            // Остановка на 1 с
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            // Остановка на 2 с
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            // Остановка на 3 с
+            Thread.Sleep(3000);
+        }
+
+        static List<User> GetListUser()
+        {
+            //Создадим несколько пользователей
+            var user1 = new User();
+            user1.Login = "user1";
+            user1.Name = "Сергей";
+            user1.IsPremium = true;
+
+            var user2 = new User();
+            user2.Login = "user2";
+            user2.Name = "Екатерина";
+            user2.IsPremium = false;
+
+            var user3 = new User();
+            user3.Login = "user3";
+            user3.Name = "Иван";
+            user3.IsPremium = true;
+
+            //Заполним список из этих пользователей
+            var listUser = new List<User>();
+            listUser.Add(user1);
+            listUser.Add(user2);
+            listUser.Add(user3);
+
+            return listUser;
         }
     }
 }
